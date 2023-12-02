4d12847e8a1768707f2acffbead36eae0a5de34d
Elmin Guliyev <elminguliyev42@gmail.com>
2023-12-02 18:59:58 +0400

    gfh
    jhjvj

---
+++ src/Main.java
@@ -0,0 +1,48 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+
+//        String str1 = "question";
+//        String str2 = "answer";
+//        int lengthstr1 = str1.length();
+//        int lengthstr2 = str2.length();
+//
+//        if(lengthstr1 < lengthstr2) {
+//            System.out.println("The first string is longer");
+//        } else if(lengthstr2 > lengthstr1){
+//            System.out.println("The second string is longer");
+//        } else {
+//            System.out.println("Both strings are of equal length");
+
+
+
+            }
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
