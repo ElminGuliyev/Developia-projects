b7771425044fc76f1475f3a735a2c380045e6f18
Elmin Guliyev <elminguliyev42@gmail.com>
2023-12-05 12:24:46 +0400

    Adding PowerFunction.java

---
+++ lesson-4/src/PowerFunction.java
@@ -0,0 +1,9 @@
+public class PowerFunction {
+    public static void main(String[]args){
+
+        int eded = 5;
+        int quvvet = 2;
+        int result = (int)Math.pow(eded,quvvet);
+        System.out.println(eded + "^" + quvvet + "=" + result);
+    }
+}
