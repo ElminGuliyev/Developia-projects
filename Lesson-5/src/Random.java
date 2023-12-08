aa75829d4260e0e5a951e6a47d1e4a944ba11dba
Elmin Guliyev <elminguliyev42@gmail.com>
2023-12-09 00:03:23 +0400

    Adding Random.java

---
+++ Lesson-5/src/Random.java
@@ -0,0 +1,20 @@
+public class Random {
+    static void calculation(int number) {
+        for (int i = 1; i <= 10; i++) {
+            int result = number * i;
+            System.out.println(number + " * " + i + " = " + result);
+
+        }
+    }
+
+
+
+        public static void main (String[]args){
+             int result = 5;
+             calculation(5);
+
+
+
+
+    }
+}
